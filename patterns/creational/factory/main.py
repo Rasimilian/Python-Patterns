346fa05b1e21c02a78b5047117d0bae01d4c8f08
Rasim <r_mamutov@mail.ru>
2023-06-04 12:10:35 +0700

    Fixed factory pattern

--- patterns/creational/factory/main.py
+++ patterns/creational/factory/main.py
@@ -1,12 +1,18 @@
-from developer_factory import JavaDeveloperFactory, PythonDeveloperFactory
+from patterns.creational.factory.developer_factory import DeveloperFactory, JavaDeveloperFactory, PythonDeveloperFactory
+
+
+def get_developer_factory(language: str) -> DeveloperFactory:
+    languages = {"Java": JavaDeveloperFactory,
+                 "Python": PythonDeveloperFactory}
+    return languages[language]()
 
 
 if __name__ == "__main__":
-    java_developer_factory = JavaDeveloperFactory()
+    java_developer_factory = get_developer_factory("Java")
     java_developer = java_developer_factory.create_developer()
     java_developer.write_code()
 
-    python_developer_factory = PythonDeveloperFactory()
+    python_developer_factory = get_developer_factory("Python")
     python_developer = python_developer_factory.create_developer()
     python_developer.write_code()
 
--- tests/creational/factory/test_factory.py
+++ tests/creational/factory/test_factory.py
@@ -1,17 +1,18 @@
 import pytest
+from patterns.creational.factory.main import get_developer_factory
 from patterns.creational.factory.developer import Developer, JavaDeveloper, PythonDeveloper
 from patterns.creational.factory.developer_factory import DeveloperFactory, JavaDeveloperFactory, PythonDeveloperFactory
 
 
 def test_factory():
-    java_developer_factory = JavaDeveloperFactory()
+    java_developer_factory = get_developer_factory("Java")
     java_developer = java_developer_factory.create_developer()
     java_developer.write_code()
     assert isinstance(java_developer_factory, DeveloperFactory)
     assert isinstance(java_developer, Developer)
     assert isinstance(java_developer, JavaDeveloper)
 
-    python_developer_factory = PythonDeveloperFactory()
+    python_developer_factory = get_developer_factory("Python")
     python_developer = python_developer_factory.create_developer()
     python_developer.write_code()
     assert isinstance(python_developer, Developer)
